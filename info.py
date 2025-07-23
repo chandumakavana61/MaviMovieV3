6e6b726c23d6959fbd54ffdf4ac9fa25831c0eab
chandumakavana61 <chandumakavana61@gmail.com>
2025-07-23 19:04:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,7 +68,7 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Movies_share_61')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Movie_share_group_61')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/movie_mela_61')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Movies_share_61') # Support Chat Link Without https:// or @
